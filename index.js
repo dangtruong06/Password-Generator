578a78d07e4b79d06ad579590ec8b0d374de3ece
dangtruong06 <DANGTT066@GMAIL.COM>
2025-01-17 16:26:07 -0800

    added first part of randomize function for users to choose which text they would want to include in password. Store checked options in allowedChars string then use random index to make a password.

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const symbols = "!@#$%^&*()_+-=:;,.<>?/|";
 const numbers = "0123456789";
 
 
-
-const max = 16;
-const min = 6;
+const max = 20;
+const min = 8;
 
 function randomNum(){
     const length = document.getElementById("length");
-
     length.value = (Math.floor(Math.random() * (max - min + 1)) + min);
 
 }
 
 function randomize(){
+    const lower = document.getElementById("lowerCase");
+    const upper = document.getElementById("upperCase");
+    const sym = document.getElementById("symBols");
+    const nums = document.getElementById("numBers");
+
+    let allowedChars = "";
+
+    allowedChars += lower.checked ? lowerChars : "";
+    allowedChars += upper.checked ? upperChars : "";
+    allowedChars += sym.checked ? symbols : "";
+    allowedChars += nums.checked ? numbers : "";
 
 }
